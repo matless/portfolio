9aa3a9f0def532fd4f726e59c02fbe9e1fac696d
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-08-28 20:59:46 +0200

    add MagicButton and TextGenerateEffect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,7 @@
+import { FaLocationArrow } from 'react-icons/fa6'
+import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -18,6 +21,21 @@ const Hero = () => {
             <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                 Dynamic Web Magic with Next.js
             </h2>
+
+            <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless Experiences"
+             />
+
+             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                Hi I&apos;m Mateusz, a Next.js Developer based in Poland.
+             </p>
+                <a href="#about">
+                    <MagicButton
+                    title="Show my work"
+                    icon={<FaLocationArrow/>}
+                    position='right' />
+                </a>
         </div>
     </div>
     </div>
