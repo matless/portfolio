6df683a1ffe701f55c5b1953b934b021f28ca4e2
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-09-22 21:36:04 +0200

    add movingBorders

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,4 +1,6 @@
+import { workExperience } from '@/data'
 import React from 'react'
+import { Button } from './ui/MovingBorders'
 
 const Experience = () => {
   return (
@@ -7,7 +9,16 @@ const Experience = () => {
         My
             <span className="text-purple"> work Experience</span>
         </h1>
-         <div className="flex flex-col items-center max-lg:mt-10">
+         <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+            {workExperience.map((card) => (
+                <Button
+                key={card.id}
+                duration={Math.floor(Math.random() * 10000) + 10000}>
+                    <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+                        <img src={card.thumbnail} alt={card.thumbnail} className='"lg:w-32 md:w-20 w-16' />
+                    </div>
+                </Button>
+            ))}
         </div>
     </div>
   )
