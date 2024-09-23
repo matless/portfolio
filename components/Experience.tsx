80578b13d56ce24d0cd4fcabbbf7b5a30323573e
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-09-23 21:16:58 +0200

    complete work experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,6 +16,14 @@ const Experience = () => {
                 duration={Math.floor(Math.random() * 10000) + 10000}>
                     <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
                         <img src={card.thumbnail} alt={card.thumbnail} className='"lg:w-32 md:w-20 w-16' />
+                        <div className="lg:ms-5">
+                            <h1 className="text-start text-xl md:text-2xl font-bold">
+                                {card.title}
+                            </h1>
+                            <p className="text-start text-white-100 mt-3 font-semibold">
+                                {card.desc}
+                            </p>
+                        </div>
                     </div>
                 </Button>
             ))}
--- data/index.ts
+++ data/index.ts
@@ -173,32 +173,26 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Junior frontend developer - BotBrainz",
+      desc: "Creating User Interfaces Using React/Next.js.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Food and Beverage Manager - Mozza Lublin",
+      desc: "Effective Budget Management, Cost Control, and Financial Performance Reporting.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Car detailer - JK Studio",
+      desc: "Car detailing, paint correction, and ceramic coating application.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
-    {
-      id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
+    
   ];
   
   export const socialMedia = [
