59e2a74e6379dd6ddd4641610e28fd076d9bd2cf
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-09-27 07:13:25 +0200

    add button with email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
@@ -15,6 +17,18 @@ const Footer = () => {
                     Ready to take <span className="text-purple">your</span> digital presence to the next level
                 </h1>
                 <p className="text-white-200 md:mt-10 text-center">Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
+                <a href="mailto:mateusz.lesniakk@gmail.com">
+                    <MagicButton
+                    title="Let's get in touch"
+                    icon={<FaLocationArrow />}
+                    position="right" />
+                </a>
+            </div>
+
+            <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+                <p className="md:text-base text-sm">
+                Copyright © 2024 Mateusz
+                </p>
             </div>
     </footer>
   )
