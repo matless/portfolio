eda0d5c32b8ad1b7efb23ca57e00d472dd33f022
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-09-28 23:38:46 +0200

    add social media

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa6'
+import { socialMedia } from '@/data'
+import { profile } from 'console'
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10 " id="contact">
-            <div className="w-full absolute left-0 -bottom-72 min-h-96">
-                <img
-                src="/footer-grid.svg"
-                alt="grid"
-                className="w-full h-full opacity-50" />
-            </div>
-
+            
             <div className="flex flex-col items-center">
                 <h1 className="heading lg:max-w-[45vw]">
                     Ready to take <span className="text-purple">your</span> digital presence to the next level
@@ -29,6 +25,14 @@ const Footer = () => {
                 <p className="md:text-base text-sm">
                 Copyright © 2024 Mateusz
                 </p>
+
+                <div className="flex items-center md:gap-3 gap-6">
+                    {socialMedia.map((profile) => (
+                        <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+                            <img src={profile.img} alt={profile.id} width={20} height={20} />
+                        </div>
+                    ))}
+                </div>
             </div>
     </footer>
   )
