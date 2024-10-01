e3f10eef8dbbca42ee9371e64d2fa6e5224c895b
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-10-01 22:48:22 +0200

    fix issue with navbar on mobile devices

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -79,7 +79,7 @@ export const BentoGridItem = ({
       )}
       style={{
         background: "rgb(4,7,29)",
-        backgroundColor:
+        backgroundImage:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
